0ff1bd54b62f8db3d1f1060c2296bf35f970014f
comunidados <musetta.alessandro@gmail.com>
2024-11-28 19:57:06 +0100

    Add sexual assault among categories

--- src/component/Header.js
+++ src/component/Header.js
@@ -17,7 +17,7 @@ class Bemvindo extends React.Component {
 
       <div className="bemvindo-box">
         <div className="bemvindo-claim">
-          <h1>Imagine andar de ônibus na madrugada ou à noite para ir <i>alimentar</i> os bairros ricos da cidade ou dar um rolezinho na praia, e depois acabar no meio de um tiroteio, ter que pular pela janela porque o ônibus está pegando fogo ou não correr o risco de se sufocar com o calor, ser parado pela polícia por conta da cor da pele ou ser assaltado.<br /><b>LINHAS</b> é um arquivo que traz as histórias dos ônibus que trabalhadorxs e moradorxs lotam todos os dias, <span style={{backgroundColor:"#66bee770"}}>vítimas do conflito armado</span>, <span style={{backgroundColor:"#66e7e170"}}>da discriminação e do racismo</span>, <span style={{backgroundColor:"#f0f55b70"}}>da negligência ou da falta de manutenção</span></h1>
+          <h1>Imagine andar de ônibus na madrugada ou à noite para ir <i>alimentar</i> os bairros ricos da cidade ou dar um rolezinho na praia, e depois acabar no meio de um tiroteio, ter que pular pela janela porque o ônibus está pegando fogo ou não correr o risco de se sufocar com o calor, ser parado pela polícia por conta da cor da pele ou ser assaltado.<br /><b>LINHAS</b> é um arquivo que traz as histórias dos ônibus que trabalhadorxs e moradorxs lotam todos os dias, <span style={{backgroundColor:"#66bee770"}}>vítimas do conflito armado</span>, <span style={{backgroundColor:"#66e7e170"}}>da discriminação e do racismo</span>, <span style={{backgroundColor:"rgba(224, 96, 248, 0.4)"}}>de assédio sexual</span>, <span style={{backgroundColor:"#f0f55b70"}}>da negligência ou da falta de manutenção</span></h1>
         </div>
       </div>
 
